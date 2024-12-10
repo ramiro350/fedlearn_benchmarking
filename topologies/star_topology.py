0e2668d4e6c80c51cc82ccc30ea72ea27929c0e9
ramiro350 <ramirofilho18@gmail.com>
2024-12-10 00:16:47 -0300

    feat: add dynamic number of hosts

--- topologies/star_topology.py
+++ topologies/star_topology.py
@@ -4,6 +4,16 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 
 def star_topology():
+    # Prompt user for the number of hosts
+    try:
+        num_hosts = int(input("Enter the number of hosts: "))
+        if num_hosts < 1:
+            print("Number of hosts must be at least 1. Setting to default value (5).")
+            num_hosts = 5
+    except ValueError:
+        print("Invalid input. Setting number of hosts to default value (5).")
+        num_hosts = 5
+
     # Create a Mininet instance
     net = Mininet(controller=Controller, switch=OVSSwitch)
 
@@ -14,7 +24,7 @@ def star_topology():
     switch = net.addSwitch('s1')
 
     # Add hosts and connect them to the switch
-    for i in range(1, 6):  # Change range for more hosts
+    for i in range(1, num_hosts + 1):
         host = net.addHost(f'h{i}')
         net.addLink(host, switch)
 
