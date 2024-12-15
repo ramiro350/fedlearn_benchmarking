f3ed937f9c5f736192f26cf1b53a97539c41d51c
ramiro350 <ramirofilho18@gmail.com>
2024-12-14 22:34:01 -0300

    wip: começa server

---
+++ Flower/server.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+import flwr as fl
+from flwr.common import Metrics
+
+# Define metric aggregation function
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
+# Define strategy
+strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+
+# Start Flower server
+fl.server.start_server(
+    server_address="10.0.0.1:8080",
+    config=fl.server.ServerConfig(num_rounds=10),
+    strategy=strategy,
+)
