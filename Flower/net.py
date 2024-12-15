606c47051492994f3f22d50605accf38b788a4cb
ramiro350 <ramirofilho18@gmail.com>
2024-12-15 17:27:13 -0300

    wip: create NN file

--- Flower/net.py
+++ Flower/net.py
@@ -33,86 +33,86 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
     
-    def load_data(partition_id: int):
-        """Load partition data."""
-        fds = FederatedDataset(dataset="cifar10", partitioners={"train": 10})
-        partition = fds.load_partition(partition_id)
-        # Divide data on each node: 80% train, 20% test
-        partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
-        pytorch_transforms = Compose(
-            [ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-        )
+def load_data(partition_id: int):
+    """Load partition data."""
+    fds = FederatedDataset(dataset="cifar10", partitioners={"train": 10})
+    partition = fds.load_partition(partition_id)
+    # Divide data on each node: 80% train, 20% test
+    partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
+    pytorch_transforms = Compose(
+        [ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
-        def apply_transforms(batch):
-            """Apply transforms to the partition from FederatedDataset."""
-            batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
-            return batch
+    def apply_transforms(batch):
+        """Apply transforms to the partition from FederatedDataset."""
+        batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
+        return batch
 
-        partition_train_test = partition_train_test.with_transform(apply_transforms)
-        trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
-        testloader = DataLoader(partition_train_test["test"], batch_size=32)
-        return trainloader, testloader
-
-
-    def train(
-        net: Net,
-        trainloader: torch.utils.data.DataLoader,
-        epochs: int,
-        device: torch.device,  # pylint: disable=no-member
-    ) -> None:
-        """Train the network."""
-        # Define loss and optimizer
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-        print(f"Training {epochs} epoch(s) w/ {len(trainloader)} batches each")
-
-        # Train the network
-        net.to(device)
-        net.train()
-        for epoch in range(epochs):  # loop over the dataset multiple times
-            running_loss = 0.0
-            for i, data in enumerate(trainloader, 0):
-                images, labels = data["img"].to(device), data["label"].to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward + backward + optimize
-                outputs = net(images)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-
-                # print statistics
-                running_loss += loss.item()
-                if i % 100 == 99:  # print every 100 mini-batches
-                    print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
+    partition_train_test = partition_train_test.with_transform(apply_transforms)
+    trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
+    testloader = DataLoader(partition_train_test["test"], batch_size=32)
+    return trainloader, testloader
 
 
-    def test(
-        net: Net,
-        testloader: torch.utils.data.DataLoader,
-        device: torch.device,  # pylint: disable=no-member
-    ) -> Tuple[float, float]:
-        """Validate the network on the entire test set."""
-        # Define loss and metrics
-        criterion = nn.CrossEntropyLoss()
-        correct, loss = 0, 0.0
+def train(
+    net: Net,
+    trainloader: torch.utils.data.DataLoader,
+    epochs: int,
+    device: torch.device,  # pylint: disable=no-member
+) -> None:
+    """Train the network."""
+    # Define loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-        # Evaluate the network
-        net.to(device)
-        net.eval()
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data["img"].to(device), data["label"].to(device)
-                outputs = net(images)
-                loss += criterion(outputs, labels).item()
-                _, predicted = torch.max(outputs.data, 1)  # pylint: disable=no-member
-                correct += (predicted == labels).sum().item()
-        accuracy = correct / len(testloader.dataset)
-        return loss, accuracy
+    print(f"Training {epochs} epoch(s) w/ {len(trainloader)} batches each")
+
+    # Train the network
+    net.to(device)
+    net.train()
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            images, labels = data["img"].to(device), data["label"].to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 100 == 99:  # print every 100 mini-batches
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+
+def test(
+    net: Net,
+    testloader: torch.utils.data.DataLoader,
+    device: torch.device,  # pylint: disable=no-member
+) -> Tuple[float, float]:
+    """Validate the network on the entire test set."""
+    # Define loss and metrics
+    criterion = nn.CrossEntropyLoss()
+    correct, loss = 0, 0.0
+
+    # Evaluate the network
+    net.to(device)
+    net.eval()
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data["img"].to(device), data["label"].to(device)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)  # pylint: disable=no-member
+            correct += (predicted == labels).sum().item()
+    accuracy = correct / len(testloader.dataset)
+    return loss, accuracy
 
 
 def main():
