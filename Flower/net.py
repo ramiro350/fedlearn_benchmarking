ab27d237849ff5c1f8b25adf720742ea7fb844c0
ramiro350 <ramirofilho18@gmail.com>
2025-01-05 15:33:59 -0300

    feat: update flower cnn

--- Flower/net.py
+++ Flower/net.py
@@ -7,35 +7,49 @@ from flwr_datasets import FederatedDataset
 from torch import Tensor
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, Normalize, ToTensor
+import pdb
 
 class Net(nn.Module):
     def __init__(self) -> None:
         super(Net, self).__init__()
+        # self.conv1 = nn.Conv2d(3, 6, 5)
+        # self.bn1 = nn.BatchNorm2d(6)
+        # self.pool = nn.MaxPool2d(2, 2)
+        # self.conv2 = nn.Conv2d(6, 16, 5)
+        # self.bn2 = nn.BatchNorm2d(16)
+        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # self.bn3 = nn.BatchNorm1d(120)
+        # self.fc2 = nn.Linear(120, 84)
+        # self.bn4 = nn.BatchNorm1d(84)
+        # self.fc3 = nn.Linear(84, 10)
         self.conv1 = nn.Conv2d(3, 6, 5)
-        self.bn1 = nn.BatchNorm2d(6)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.bn2 = nn.BatchNorm2d(16)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.bn3 = nn.BatchNorm1d(120)
         self.fc2 = nn.Linear(120, 84)
-        self.bn4 = nn.BatchNorm1d(84)
         self.fc3 = nn.Linear(84, 10)
+        # pdb.set_trace()
 
     # pylint: disable=arguments-differ,invalid-name
     def forward(self, x: Tensor) -> Tensor:
         """Compute forward pass."""
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.bn3(self.fc1(x)))
-        x = F.relu(self.bn4(self.fc2(x)))
+        # x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        # x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        # x = x.view(-1, 16 * 5 * 5)
+        # x = F.relu(self.bn3(self.fc1(x)))
+        # x = F.relu(self.bn4(self.fc2(x)))
+        # x = self.fc3(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
     
 def load_data(partition_id: int):
     """Load partition data."""
-    fds = FederatedDataset(dataset="cifar10", partitioners={"train": 10})
+    fds = FederatedDataset(dataset="cifar10", partitioners={"train": 4})
     partition = fds.load_partition(partition_id)
     # Divide data on each node: 80% train, 20% test
     partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
